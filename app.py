e4cebf0b334d4d0cf5597d2de552c713b0ce4485
L0uko <touko.t.toivonen@gmail.com>
2024-03-23 15:07:54 +0200

    add index.html to main page

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!. Tämä on testi. "
+    return render_template(index.html)
 
 @app.route("/new1")
 def index():
