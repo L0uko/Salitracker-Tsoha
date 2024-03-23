fe0fe37c6024f8c6a32e57168ef94ab420a22c4e
L0uko <touko.t.toivonen@gmail.com>
2024-03-23 15:33:48 +0200

    add result.html

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def index():
 @app.route("/form")
 def form():
     return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    return render_template("result.html")
