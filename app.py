495422498471541a92553b5eca793c5f01eba1c5
L0uko <touko.t.toivonen@gmail.com>
2024-03-25 15:58:32 +0200

    wip login

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ app.secret_key = getenv("SECRET_KEY")
 def index():
     return render_template("index.html")
 
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+
 @app.route("/form")
 def form():
     return render_template("form.html")
