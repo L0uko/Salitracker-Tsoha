7c29ec33463848df44209bb2529c6d7da94f8238
L0uko <touko.t.toivonen@gmail.com>
2024-04-05 14:45:42 +0300

    Add more tables and more options in form.html

--- schema.sql
+++ schema.sql
@@ -1,1 +1,24 @@
-Create TABLE visits (id SERIAL PRIMARY KEY, exersise TEXT, sets INTEGER);
+Create TABLE visits (
+id SERIAL PRIMARY KEY,
+exersise_id REFERENCES exersise(id),
+time CURRENT_TIME
+);
+CREATE TABLE excersise (
+id SERIAL PRIMARY KEY, 
+excersename_id REFERENCES excersisename(id), 
+sets INTEGER, 
+weight INTEGER
+);  
+CREATE TABLE excersisename (
+id SERIAL PRIMARY KEY, 
+name TEXT
+);
+Create TABLE users (
+id SERIAL PRIMARY KEY, 
+user_id REFERENCES user(id
+);
+CREATE TABLE user (
+id SERIAL PRIMARY KEY, 
+name TEXT, 
+password TEXT
+);
