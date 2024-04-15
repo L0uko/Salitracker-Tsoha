209495585121c52fbb05793dd014c011361bc602
L0uko <touko.t.toivonen@gmail.com>
2024-04-15 14:28:33 +0300

    Refactoring success.

---
+++ users.py
@@ -0,0 +1,12 @@
+from db import db
+from sqlalchemy import text
+
+
+
+def find_user_id(username):
+    try:
+        sql    = text('''SELECT id FROM users WHERE username = :username''')
+        user_id= db.session.execute(sql, {"username":username}).fetchone()
+        return user_id[0]
+    except:
+        return
